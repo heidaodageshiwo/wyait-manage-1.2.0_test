bbabce82864420a584ce595f70a7796dce872768
zhangqiang <heidaodageshiwo@163.com>
2019-09-07 20:40:52 +0800

    初次提交1.2——test 1.可以打war包，进行配置（<Context docBase="G:\tomcat\apache-tomcat-8.5.28\webapps\项目名称.war" path="" debug="0" reloadable="true"  /> ）

--- src/main/java/com/wyait/manage/ServletInitializer.java
+++ src/main/java/com/wyait/manage/ServletInitializer.java
@@ -1,15 +1,14 @@
-package com.wyait.manage;/*
 package com.wyait.manage;
 
+import com.wyait.manage.WyaitManageApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(WyaitManageApplication.class);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WyaitManageApplication.class);
+    }
 
 }
-*/
